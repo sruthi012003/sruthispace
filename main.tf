7503d4a539e46a0b7fe45aed5cdc2dc933cb2767
sruthi012003 <121019442+sruthi012003@users.noreply.github.com>
2025-10-31 10:27:53 +0530

    main.tf

--- main.tf
+++ main.tf
@@ -1,17 +1,20 @@
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Action": "sts:AssumeRole",
-      "Condition": {
-        "StringLike": {
-          "sts:ExternalId": "sruthi012003@*"
-        }
-      },
-      "Effect": "Allow",
-      "Principal": {
-        "AWS": "324880187172"
-      }
-    }
-  ]
+provider "aws" {
+
+region = "ap-south-1"
+
 }
+
+resource "aws_instance" "example" {
+
+ami           = "ami-02d26659fd82cf299" # Example Ubuntu AMI for ap-south-1
+
+instance_type = "t3.micro"
+
+tags = {
+
+   Name = "Terraform-EC2"
+
+}
+
+}
+ 
